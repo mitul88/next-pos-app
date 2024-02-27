ecb254c73cdb524d3ef9d803cd9a23d29ccd9ddd
mitul88 <shahed.rahman88@gmail.com>
2024-02-27 13:07:26 +0600

    product item created

---
+++ src/components/pos/ProductItem.jsx
@@ -0,0 +1,18 @@
+
+const ProductItem = ({title, image, price}) => {
+  return (
+    <div className='rounded bg-white border-2 border-[#B4B4B8] text-center'>
+        <div className="h-48">
+            <img src={image} alt="" className="h-full w-full"/>
+        </div>
+        <div className='bg-[#DCF2F1]'>
+            <span className='font-bold text-[#638889]'>${price}</span>
+        </div>
+        <div className="border border-t-[#B4B4B8]">
+            <span className='text-sm font-bold text-[#638889]'>{title}</span>
+        </div>
+    </div>
+  )
+}
+
+export default ProductItem
--- src/components/pos/ProductSection.jsx
+++ src/components/pos/ProductSection.jsx
@@ -3,12 +3,14 @@ import SearchProducts from './SearchProducts'
 import LinkButton from './LinkButton'
 
 import { BsThreeDotsVertical } from "react-icons/bs";
+import ProductItem from './ProductItem';
+import { products } from '../../../mock/products';
 
 const ProductSection = () => {
   return (
     <div className='pb-1 bg-[#DCF2F1]'>
         <SearchProducts />
-        <div className='mt-3 flex items-center justify-between'>
+        <div className='mt-3 px-4 flex items-center justify-between'>
           <LinkButton text="All Categories" color="#B4B4B3" solid="white"/>
           <LinkButton text="Electronics" color="#B4B4B3" solid="white"/>
           <LinkButton text="Men Fashion" color="#B4B4B3" solid="white"/>
@@ -16,6 +18,12 @@ const ProductSection = () => {
           <button>
             <BsThreeDotsVertical size={25}/>
           </button>
+        </div>
+
+        <div className='w-full p-4 grid grid-cols-4 gap-4'>
+          {products.map((item, index)=> (
+            <ProductItem title={item.title} image={item.img} price={item.price} key={item.id} />
+          ))}
         </div>
     </div>
   )
