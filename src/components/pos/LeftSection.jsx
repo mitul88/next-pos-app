be47fff6826b60dd52b80637dead2ab1f3ec3e04
mitul88 <shahed.rahman88@gmail.com>
2024-02-27 12:10:16 +0600

    category section added

--- src/components/pos/LeftSection.jsx
+++ src/components/pos/LeftSection.jsx
@@ -15,10 +15,10 @@ const LeftSection = () => {
     <div>
         <div className='py-2  flex items-center justify-between'>
             <button className="px-3"><RxHamburgerMenu size={25} /></button>
-            <LinkButton icon={<SlNote size={25} />} text="Note" link="https://www.facebook.com" />
-            <LinkButton icon={<BsTruck size={25}/>} text="Shipping" link="https://www.google.com" />
-            <LinkButton icon={<IoIosTimer size={25}/>} text="Hold Orders" link="https://www.google.com" />
-            <LinkButton icon={<IoIosAddCircle size={25} />} text="New Item" link="https://www.twitter.com" />
+            <LinkButton icon={<SlNote size={25} />} text="Note" solid={true} link="https://www.facebook.com" />
+            <LinkButton icon={<BsTruck size={25}/>} text="Shipping" solid={true} link="https://www.google.com" />
+            <LinkButton icon={<IoIosTimer size={25}/>} text="Hold Orders" solid={true} link="https://www.google.com" />
+            <LinkButton icon={<IoIosAddCircle size={25} />} text="New Item" solid={true} link="https://www.twitter.com" />
         </div>
 
         <div className="pb-2">
--- src/components/pos/ProductSection.jsx
+++ src/components/pos/ProductSection.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import SearchProducts from './SearchProducts'
+import LinkButton from './LinkButton'
+
+import { BsThreeDotsVertical } from "react-icons/bs";
 
 const ProductSection = () => {
   return (
     <div className='py-1'>
         <SearchProducts />
+        <div className='mt-3 flex items-center justify-between'>
+          <LinkButton text="All Categories" color="#B4B4B3"/>
+          <LinkButton text="Electronics" color="#B4B4B3"/>
+          <LinkButton text="Men Fashion" color="#B4B4B3"/>
+          <LinkButton text="Women Fashion" color="#B4B4B3"/>
+          <button>
+            <BsThreeDotsVertical size={25}/>
+          </button>
+        </div>
     </div>
   )
 }
