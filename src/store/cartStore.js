5cd6235f69b3ddcc5e9415758d19c765d7e90fe9
mitul88 <shahed.rahman88@gmail.com>
2024-02-28 00:13:34 +0600

    cart store created

---
+++ src/store/cartStore.js
@@ -0,0 +1,11 @@
+import { create } from "zustand";
+
+
+const useCartStore = create((set) => ({
+    items: [],
+    addToCart: (item) => set((state) => ({ items: [...state.items, item] })),
+    removeFromCart: (itemId) =>
+      set((state) => ({ items: state.items.filter((item) => item.id !== itemId) })),
+  }));
+  
+  export default useCartStore;
