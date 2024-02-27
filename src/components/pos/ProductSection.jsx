fce5e8409fac76ccb0933c5953d2dd4ea9451658
mitul88 <shahed.rahman88@gmail.com>
2024-02-27 12:40:27 +0600

    btn bg color fix

--- src/components/pos/ProductSection.jsx
+++ src/components/pos/ProductSection.jsx
@@ -6,13 +6,13 @@ import { BsThreeDotsVertical } from "react-icons/bs";
 
 const ProductSection = () => {
   return (
-    <div className='py-1'>
+    <div className='pb-1 bg-[#DCF2F1]'>
         <SearchProducts />
         <div className='mt-3 flex items-center justify-between'>
-          <LinkButton text="All Categories" color="#B4B4B3"/>
-          <LinkButton text="Electronics" color="#B4B4B3"/>
-          <LinkButton text="Men Fashion" color="#B4B4B3"/>
-          <LinkButton text="Women Fashion" color="#B4B4B3"/>
+          <LinkButton text="All Categories" color="#B4B4B3" solid="white"/>
+          <LinkButton text="Electronics" color="#B4B4B3" solid="white"/>
+          <LinkButton text="Men Fashion" color="#B4B4B3" solid="white"/>
+          <LinkButton text="Women Fashion" color="#B4B4B3" solid="white"/>
           <button>
             <BsThreeDotsVertical size={25}/>
           </button>
