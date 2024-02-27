a3dac17dab1df352b62063366f412c06a4febc1e
mitul88 <shahed.rahman88@gmail.com>
2024-02-28 01:02:04 +0600

    design fix

--- src/components/pos/PosCart.jsx
+++ src/components/pos/PosCart.jsx
@@ -19,29 +19,29 @@ const PosCart = () => {
   return (
     <div className="w-full flex flex-col">
 
-        {items.map((item) => (
+        {items.map((item, index) => (
             <div className="flex items-center justify-between" key={item.id}>
                 <div className="basis-1/12">
                     <FaRegEdit size={25}/>
                 </div>
-                <div className="basis-10/12 px-3 py-1 flex items-center justify-between border-t border-l border-r border-t-[#C7C8CC] border-l-[#C7C8CC] border-r-[#C7C8CC] rounded-t flex items-center w-max">
-                    <div>
+                <div className={`basis-10/12 px-3 py-1 flex items-center justify-between border-t border-l border-r border-t-[#C7C8CC] border-l-[#C7C8CC] border-r-[#C7C8CC] flex items-center w-max ${index == 0 && "rounded-t"}`}>
+                    <div className="w-3/6">
                         <span className="text-md lg:text-lg">{item.title}</span>
                     </div>
-                    <div>
-                        <span className="text-md lg:text-lg">& {item.price}</span>
+                    <div className="w-1/6">
+                        <span className="text-md lg:text-lg">${item.price}</span>
                     </div>
-                    <div className="flex items-center text-[#B4B4B3]">
+                    <div className="w-1/6 flex items-center text-[#B4B4B3]">
                         <button><FaMinusCircle size={20}/></button>
                         <span className="font-bold mx-3">1</span>
                         <button><FaPlusCircle size={20}/></button>
                     </div>
-                    <div>
+                    <div className="w-1/6">
                         <span className="text-md lg:text-lg">& 92.00</span>
                     </div>
                 </div>
                 <div className="basis-1/12 flex justify-end">
-                    <FaRegTrashCan onClick={()=>handleRemoveFromCart(item.id)} size={25} className="text-red-600"/>
+                    <FaRegTrashCan onClick={()=>handleRemoveFromCart(item.id)} size={25} className="text-red-600 cursor-pointer"/>
                 </div>
             </div>
         ))}
--- src/store/cartStore.js
+++ src/store/cartStore.js
@@ -5,6 +5,8 @@ const useCartStore = create((set) => ({
     items: [],
     addToCart: (item) => set((state) => ({ items: [...state.items, item] })),
     removeFromCart: (itemId) => set((state) => ({ items: state.items.filter((item) => item.id !== itemId) })),
+    increaseItem: (itemId) =>set((state) => ({})),
+    decreaseItem: (itemId) =>set((state) => ({})),
   }));
   
   export default useCartStore;
